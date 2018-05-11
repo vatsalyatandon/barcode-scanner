5c563a0ec11883691d89d66483d1801809035166
Vatsalya Tandon <vatsalyatandon@gmail.com>
2018-05-11 13:03:58 +0530

    Added Code

---
+++ barcode.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+from pyzbar.pyzbar import pyzbar
+import numpy as np
+import cv2
+
+def decode(im) :
+	decodedObjects = decode(im)
+
+	for obj in decodedObjects:
+		print('Type : ', obj.type)
+		print('Data : ', obj.data, '\n')
+	return decodedObjects
+
+def display(im, decodedObjects) :
+	for decodedObject in decodedObjects:
+		points = decodedObject.location
+
+		if len(points) > 4 :
+			hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+			hull = list(map(tuple, np.squeeze(hull)))
+		else :
+			hull = points
+
+		n= len(hull)
+
+		for j in range(0,n):
+			cv2.line(im, hull[j], hull[(j+1)%n], (255,0,0),3)
+
+	cv2.imshow("Results", im);
+	cv2.waitKey(0);
+
+if __name__ == '__main__':
+
+	im = cv2.imread('zbar-test.jpg')
+
+	decodedObjects = decode(im)
+	display(im,decodedObjects)
